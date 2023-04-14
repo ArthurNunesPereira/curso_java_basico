719f4d6220666d5900e4d267c4bdb57020c6f843
Arthur Nunes Pereira <arthurnunesp@live.com>
2023-04-14 11:57:53 -0300

    comiit 14/04/2023

--- src/exercicios_aulas25_27/JogoDaVelha.java
+++ src/exercicios_aulas25_27/JogoDaVelha.java
@@ -2,94 +2,51 @@ package exercicios_aulas25_27;
 
 public class JogoDaVelha {
 
-	/* A FAZER
-	 Scanner scan = new Scanner(System.in);
+	char jogoDaVelha[][] = new char[3][3];
+	int jogada = 1;
 
-		char jogoDaVelha[][] = new char[3][3];
-		boolean vencedor = false;
-		int jogada = 1;
-		char sinal;
-		int linha = 0, coluna = 0; // Declarando duas variavéis na mesma linha.
+	boolean validarJogada(int linha, int coluna, char sinal) {
+
+		if (jogoDaVelha[linha][coluna] == sinal || jogoDaVelha[linha][coluna] == 'O') {
+			return false;
+		} else {
+			jogoDaVelha[linha][coluna] = sinal;
+			jogada++;
+			return true;
+		}
+	}
+
+	void imprimirTabuleiro() {
 
 		System.out.println("---Jogo da Velha---");
-		System.out.println("Player 1 - [X].");
-		System.out.println("Player 2 - [O].");
-		while (!vencedor) {
-
-			if (jogada % 2 != 0) {
-				System.out.println("Vez do player [X]. Escolha uma linha e coluna (1 - 3).");
-				sinal = 'X';
-			} else {
-				System.out.println("Vez do player [O]. Escolha uma linha e coluna (1 - 3).");
-				sinal = 'O';
+		for (int i = 0; i < jogoDaVelha.length; i++) {
+			System.out.print("   | ");
+			for (int j = 0; j < jogoDaVelha[i].length; j++) {
+				System.out.print(jogoDaVelha[i][j] + " | ");
 			}
+			System.out.println();
+		}
+	}
 
-			boolean linhaValida = false;
-			while (!linhaValida) {
-				System.out.println("Selecione uma linha (1 - 3):");
-				linha = scan.nextInt();
+	boolean verificarGanhador(char sinal) {
 
-				if (linha >= 1 && linha <= 3) {
-					linhaValida = true;
-				} else {
-					System.out.println("A linha selecionada é inválida, por favor tente novamente!");
-				}
-			}
-
-			boolean colunaValida = false;
-			while (!colunaValida) {
-				System.out.println("Selecione uma coluna:");
-				coluna = scan.nextInt();
-
-				if (coluna >= 1 && coluna <= 3) {
-					colunaValida = true;
-				} else {
-					System.out.println("A coluna selecionada é inválida, por favor tente novamente!");
-				}
-			}
-
-			linha--;
-			coluna--;
-			if (jogoDaVelha[linha][coluna] == 'X' || jogoDaVelha[linha][coluna] == 'O') {
-				System.out.println("Este espaço ja está preenchido, por favor tente novamente!");
-			} else {
-				jogoDaVelha[linha][coluna] = sinal;
-				jogada++;
-			}
-
-			System.out.println("---Jogo da Velha---");
-			for (int i = 0; i < jogoDaVelha.length; i++) {
-				System.out.print("   | ");
-				for (int j = 0; j < jogoDaVelha[i].length; j++) {
-					System.out.print(jogoDaVelha[i][j] + " | ");
-				}
-				System.out.println();
-			}
-
-			// Checar se alguém ganhou
-			if ((jogoDaVelha[0][0] == 'X' && jogoDaVelha[0][1] == 'X' && jogoDaVelha[0][2] == 'X') ||     // Linha 1
-					(jogoDaVelha[1][0] == 'X' && jogoDaVelha[1][1] == 'X' && jogoDaVelha[1][2] == 'X') || // Linha 2
-					(jogoDaVelha[2][0] == 'X' && jogoDaVelha[2][1] == 'X' && jogoDaVelha[2][2] == 'X') || // Linha 3
-					(jogoDaVelha[0][0] == 'X' && jogoDaVelha[1][0] == 'X' && jogoDaVelha[2][0] == 'X') || // Coluna 1
-					(jogoDaVelha[0][1] == 'X' && jogoDaVelha[1][1] == 'X' && jogoDaVelha[2][1] == 'X') || // Coluna 2
-					(jogoDaVelha[0][2] == 'X' && jogoDaVelha[1][2] == 'X' && jogoDaVelha[2][2] == 'X') || // Coluna 3
-					(jogoDaVelha[0][0] == 'X' && jogoDaVelha[1][1] == 'X' && jogoDaVelha[2][2] == 'X') || // Diagonal 1
-					(jogoDaVelha[2][0] == 'X' && jogoDaVelha[1][1] == 'X' && jogoDaVelha[0][2] == 'X')){  // Diagonal 2
-				vencedor = true;
-				System.out.println("O vencedor foi o Player [X]!");
-			} else if ((jogoDaVelha[0][0] == 'O' && jogoDaVelha[0][1] == 'O' && jogoDaVelha[0][2] == 'O') || // Linha 1
-					(jogoDaVelha[1][0] == 'O' && jogoDaVelha[1][1] == 'O' && jogoDaVelha[1][2] == 'O') || 	// Linha 2
-					(jogoDaVelha[2][0] == 'O' && jogoDaVelha[2][1] == 'O' && jogoDaVelha[2][2] == 'O') || 	// Linha 3
-					(jogoDaVelha[0][0] == 'O' && jogoDaVelha[1][0] == 'O' && jogoDaVelha[2][0] == 'O') || 	// Coluna 1
-					(jogoDaVelha[0][1] == 'O' && jogoDaVelha[1][1] == 'O' && jogoDaVelha[2][1] == 'O') || 	// Coluna 2
-					(jogoDaVelha[0][2] == 'O' && jogoDaVelha[1][2] == 'O' && jogoDaVelha[2][2] == 'O') || 	// Coluna 3
-					(jogoDaVelha[0][0] == 'O' && jogoDaVelha[1][1] == 'O' && jogoDaVelha[2][2] == 'O') || 	// Diagonal 1
-					(jogoDaVelha[2][0] == 'O' && jogoDaVelha[1][1] == 'O' && jogoDaVelha[0][2] == 'O')){  	// Diagonal 2
-				vencedor = true;
-				System.out.println("O vencedor foi o Player [O]!");
-			} else if (jogada > 9) {
-				vencedor = true;
-				System.out.println("Nenhum vencedor, deu velha!");
-			}
-		}*/
+		if ((jogoDaVelha[0][0] == sinal && jogoDaVelha[0][1] == sinal && jogoDaVelha[0][2] == sinal) || // Linha 1
+			(jogoDaVelha[1][0] == sinal && jogoDaVelha[1][1] == sinal && jogoDaVelha[1][2] == sinal) || // Linha 2
+			(jogoDaVelha[2][0] == sinal && jogoDaVelha[2][1] == sinal && jogoDaVelha[2][2] == sinal) || // Linha 3
+			(jogoDaVelha[0][0] == sinal && jogoDaVelha[1][0] == sinal && jogoDaVelha[2][0] == sinal) || // Coluna 1
+			(jogoDaVelha[0][1] == sinal && jogoDaVelha[1][1] == sinal && jogoDaVelha[2][1] == sinal) || // Coluna 2
+			(jogoDaVelha[0][2] == sinal && jogoDaVelha[1][2] == sinal && jogoDaVelha[2][2] == sinal) || // Coluna 3
+			(jogoDaVelha[0][0] == sinal && jogoDaVelha[1][1] == sinal && jogoDaVelha[2][2] == sinal) || // Diagonal 1
+			(jogoDaVelha[2][0] == sinal && jogoDaVelha[1][1] == sinal && jogoDaVelha[0][2] == sinal)) { // Diagonal 2
+			return true;
+		}
+		return false;
+	}
+	
+	boolean vezJogador1() {
+		if (jogada % 2 != 0) {
+			return true;
+		}
+		return false;
+	}
 }
---
+++ src/exercicios_aulas28/Pacotes.java
@@ -0,0 +1,35 @@
+package exercicios_aulas28;
+
+import curso_java_basico_aula27.Carro; // Importando a classe Carro da aula 27
+import exercicios_aulas20.ex006; // Importando o ex06 do pacote de exercicios da aula 20
+import exercicios_aulas20.ex005; // Importando o ex05 do pacote de exercicios da aula 20
+
+public class Pacotes {
+
+	public static void main(String[] args) {
+		
+		Carro carro;
+		
+		ex006 ex006;
+		ex005 ex005;
+		
+		//Para ver mais sobre imports, consultar o link: https://docs.oracle.com/en/java/javase/17/docs/api/
+		
+		/*Exemplos de Imports: 
+		
+		 Import de bibliotecas Java:
+		 import java.util.Scanner;
+		 import java.text.DecimalFormat; 
+		 
+		 Import de Classes:
+		 import curso_java_basico_aula27.Carro; // Importando a classe Carro da aula 27
+		 import exercicios_aulas20.ex006;
+		 
+		 
+		 Import de um framework:
+		 import org.springframework.data.annotation.Id;
+		 import org.springframework.data.mongodb.core.mapping.Document;
+		 */
+	}
+
+}
